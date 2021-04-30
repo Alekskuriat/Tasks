ae0767c20afcff1c9018fcf930118f83a710e4c0
Aleks <kuriatae@mail.ru>
2021-04-30 23:12:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tasks/DetailsNote.java
@@ -0,0 +1,22 @@
+package com.example.tasks;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class DetailsNote extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_detail, container, false);
+    }
+}
